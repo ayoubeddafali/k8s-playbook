f4eb9e92d8f7fdb10c670927d73a780fcd7b7ccb
ayoubensalem <ayoubensalem@gmail.com>
2018-09-30 16:01:33 +0100

    updates

--- main.sh
+++ main.sh
@@ -1,9 +1,6 @@
-python create_all.py
 
 export ANSIBLE_HOST_KEY_CHECKING=False
 export ANSIBLE_SSH_RETRIES=5
 
-ansible-playbook -i hosts kube-dependencies.yml
-ansible-playbook -i hosts master.yml
-ansible-playbook -i hosts workers.yml
+ansible-playbook -i hosts main.yml
 
